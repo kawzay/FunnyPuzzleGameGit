4ada6dfc8caa83762e9d7101f3353f752fad8c10
kawzay <kawzay@gmail.com>
2022-05-28 05:11:38 +0500

    отображение уровней 1

--- Assets/Scripts/BiomForestLevels.cs
+++ Assets/Scripts/BiomForestLevels.cs
@@ -12,6 +12,7 @@ public class BiomForestLevels : MonoBehaviour
     public Button lvl4;
     public Sprite unlock;
     public Sprite resolvedLevel1;
+    public Sprite resolvedLevel2;
     int levelComplete;
 
     void Start()
@@ -21,33 +22,65 @@ public class BiomForestLevels : MonoBehaviour
         lvl2.interactable = false;
         lvl3.interactable = false;
         lvl4.interactable = false;
-        //lvl2.image.color = new Color(0f, 0f, 0f, .0f);
-        //lvl3.image.color = new Color(0f, 0f, 0f, .0f);
-        //lvl4.image.color = new Color(0f, 0f, 0f, .0f);
 
+        var cc = lvl2.image.color;
+        cc.a = 0f;
+        lvl2.image.color = cc;
+        cc = lvl3.image.color;
+        cc.a = 0f;
+        lvl3.image.color = cc;
+        cc = lvl4.image.color;
+        cc.a = 0f;
+        lvl4.image.color = cc;
 
 
         switch (levelComplete)
         {
             case 1:
                 lvl2.interactable = true;
+
+                cc = lvl2.image.color;
+                cc.a = 1f;
+                lvl2.image.color = cc;
+
                 lvl1.image.sprite = resolvedLevel1;
+                lvl2.image.sprite = unlock;
                 break;
             case 2:
                 lvl2.interactable = true;
                 lvl3.interactable = true;
+
+                cc = lvl2.image.color;
+                cc.a = 1f;
+                lvl2.image.color = cc;
+
+                cc = lvl3.image.color;
+                cc.a = 1f;
+                lvl3.image.color = cc;
+
                 lvl1.image.sprite = resolvedLevel1;
-                //lvl4.image.color = new Color(0f, 0f, 0f, .100f);
-                lvl2.image.sprite = resolvedLevel1;
+                lvl2.image.sprite = resolvedLevel2;
                 lvl3.image.sprite = unlock;
                 break;
             case 3:
                 lvl2.interactable = true;
                 lvl3.interactable = true;
                 lvl4.interactable = true;
+
+                cc = lvl2.image.color;
+                cc.a = 1f;
+                lvl2.image.color = cc;
+
+                cc = lvl3.image.color;
+                cc.a = 1f;
+                lvl3.image.color = cc;
+
+                cc = lvl4.image.color;
+                cc.a = 1f;
+                lvl4.image.color = cc;
+
                 lvl1.image.sprite = resolvedLevel1;
-                lvl2.image.sprite = resolvedLevel1;
-                lvl2.image.sprite = resolvedLevel1;
+                lvl2.image.sprite = resolvedLevel2;
                 lvl3.image.sprite = resolvedLevel1;
                 lvl4.image.sprite = unlock;
                 break;
@@ -55,9 +88,21 @@ public class BiomForestLevels : MonoBehaviour
                 lvl2.interactable = true;
                 lvl3.interactable = true;
                 lvl4.interactable = true;
+
+                cc = lvl2.image.color;
+                cc.a = 1f;
+                lvl2.image.color = cc;
+
+                cc = lvl3.image.color;
+                cc.a = 1f;
+                lvl3.image.color = cc;
+
+                cc = lvl4.image.color;
+                cc.a = 1f;
+                lvl4.image.color = cc;
+
                 lvl1.image.sprite = resolvedLevel1;
-                lvl2.image.sprite = resolvedLevel1;
-                lvl2.image.sprite = resolvedLevel1;
+                lvl2.image.sprite = resolvedLevel2;
                 lvl3.image.sprite = resolvedLevel1;
                 lvl4.image.sprite = resolvedLevel1;
                 break;
