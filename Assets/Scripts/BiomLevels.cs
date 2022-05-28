be9da6674952aa9b49a43d0a7a8b3366bf1bd36a
kawzay <kawzay@gmail.com>
2022-05-28 08:38:20 +0500

    исправления скриптов

---
+++ Assets/Scripts/BiomLevels.cs
@@ -0,0 +1,133 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BiomLevels : MonoBehaviour
+{
+    public Button lvl1;
+    public Button lvl2;
+    public Button lvl3;
+    public Button lvl4;
+    public Sprite unlock;
+    public Sprite resolvedLevel1;
+    public Sprite resolvedLevel2;
+    int levelComplete;
+    bool isCompleted = false;
+
+
+
+    void Start()
+    {
+        levelComplete = PlayerPrefs.GetInt("LevelComplete");
+
+        var cc = lvl2.image.color;
+        if (!isCompleted)
+        {
+            lvl2.interactable = false;
+            lvl3.interactable = false;
+            lvl4.interactable = false;
+
+            cc = lvl2.image.color;
+            cc.a = 0f;
+            lvl2.image.color = cc;
+            cc = lvl3.image.color;
+            cc.a = 0f;
+            lvl3.image.color = cc;
+            cc = lvl4.image.color;
+            cc.a = 0f;
+            lvl4.image.color = cc;
+        }
+
+        if (levelComplete == 1)
+        {
+            lvl2.interactable = true;
+
+            cc = lvl2.image.color;
+            cc.a = 1f;
+            lvl2.image.color = cc;
+
+            lvl1.image.sprite = resolvedLevel1;
+            lvl2.image.sprite = unlock;
+        }
+        if (levelComplete == 2)
+        {
+            lvl2.interactable = true;
+            lvl3.interactable = true;
+
+            cc = lvl2.image.color;
+            cc.a = 1f;
+            lvl2.image.color = cc;
+
+            cc = lvl3.image.color;
+            cc.a = 1f;
+            lvl3.image.color = cc;
+
+            lvl1.image.sprite = resolvedLevel1;
+            lvl2.image.sprite = resolvedLevel2;
+            lvl3.image.sprite = unlock;
+        }
+        if (levelComplete == 3)
+        {
+            lvl2.interactable = true;
+            lvl3.interactable = true;
+            lvl4.interactable = true;
+
+            cc = lvl2.image.color;
+            cc.a = 1f;
+            lvl2.image.color = cc;
+
+            cc = lvl3.image.color;
+            cc.a = 1f;
+            lvl3.image.color = cc;
+
+            cc = lvl4.image.color;
+            cc.a = 1f;
+            lvl4.image.color = cc;
+
+            lvl1.image.sprite = resolvedLevel1;
+            lvl2.image.sprite = resolvedLevel2;
+            lvl3.image.sprite = resolvedLevel1;
+            lvl4.image.sprite = unlock;
+        }
+        if (levelComplete >= 4)
+        {
+            isCompleted = true;
+            lvl2.interactable = true;
+            lvl3.interactable = true;
+            lvl4.interactable = true;
+
+            cc = lvl2.image.color;
+            cc.a = 1f;
+            lvl2.image.color = cc;
+
+            cc = lvl3.image.color;
+            cc.a = 1f;
+            lvl3.image.color = cc;
+
+            cc = lvl4.image.color;
+            cc.a = 1f;
+            lvl4.image.color = cc;
+
+            lvl1.image.sprite = resolvedLevel1;
+            lvl2.image.sprite = resolvedLevel2;
+            lvl3.image.sprite = resolvedLevel1;
+            lvl4.image.sprite = resolvedLevel1;
+        }
+    }
+
+    public void LoadTo(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void Reset()
+    {
+        lvl2.interactable = false;
+        lvl3.interactable = false;
+        lvl4.interactable = false;
+        PlayerPrefs.DeleteAll();
+    }
+}
+
